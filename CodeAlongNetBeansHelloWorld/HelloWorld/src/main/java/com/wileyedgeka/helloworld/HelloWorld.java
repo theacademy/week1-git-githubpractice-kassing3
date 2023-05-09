31cd3f8c84fddf48dfa5b13267a1a14b28f2930b
kassing3 <108312371+kassing3@users.noreply.github.com>
2023-05-09 14:27:28 -0400

    Hello World in NetBeans

---
+++ CodeAlongNetBeansHelloWorld/HelloWorld/src/main/java/com/wileyedgeka/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Korinna Assing
+ * @purpose: My first Hello World program in an IDE
+ */
+
+package com.wileyedgeka.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Korinna!");
+    }
+}
